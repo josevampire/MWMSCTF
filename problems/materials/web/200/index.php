7d810cfbe945f7228bddba00d5a2916df15847df
JoseVampire <nicholas.marcuzzo@gmail.com>
2015-08-31 20:55:17 -0500

    Fix web200

--- problems/materials/web/200/index.php
+++ problems/materials/web/200/index.php
@@ -1,3 +1,8 @@
+<?php
+	if (!isset($_GET['admin'])) {
+		header("Location: index.php?admin=FALSE");
+	}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,16 +12,12 @@
 	<body>
     <h1 style="margin:10px">Super Secret Stuff Login Page</h1>
     <?php
-      if (isset($_GET['admin'])) {
-        if ($_GET['admin'] == 'FALSE') {
-          echo '<h4 class="text-danger" style="margin:10px">No admin rights. Please login.</h4>';
-        } else if ($_GET['admin'] == 'TRUE') {
-          echo '<h4 class="text-success" style="margin: 0px 0px 0px 10px">Welcome administrator!</h4><br><h5 class="text-success" style="margin: 0px 0px 10px 10px">flag("broDoYouEvenGetVar")</h5>';
-        } else {
-          echo '<h4 class="text-warning" style="margin:10px">Unknow permission. Admin var does not match specs.</h4>';
-        }
+			if ($_GET['admin'] == 'FALSE') {
+        echo '<h4 class="text-danger" style="margin:10px">No admin rights. Please login.</h4>';
+      } else if ($_GET['admin'] == 'TRUE') {
+        echo '<h4 class="text-success" style="margin: 0px 0px 0px 10px">Welcome administrator!</h4><br><h5 class="text-success" style="margin: 0px 0px 10px 10px">flag("broDoYouEvenGetVar")</h5>';
       } else {
-        header("Location: index.php?admin=FALSE");
+        echo '<h4 class="text-warning" style="margin:10px">Unknow permission. Admin var does not match specs.</h4>';
       }
     ?>
     <form style="width:150px">
